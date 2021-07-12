a0ee6e8cd4bf61cfb9a41b8932f785986bae4be2
Oleh <ohapon@Olehs-MacBook-Pro.local>
2021-07-12 15:00:47 +0300

    Refactored unit tests

--- src/test/java/com/ohapon/datastructures/list/AbstractListTest.java
+++ src/test/java/com/ohapon/datastructures/list/AbstractListTest.java
@@ -7,24 +7,29 @@ public abstract class AbstractListTest {
 
     abstract List createList();
 
-    @Test
-    public void testAddElement() {
-
+    List createListWithThreeElements() {
         List list = createList();
-        assertEquals(0, list.size());
 
         list.add("Element 0");
         list.add("Element 1");
         list.add("Element 2");
+
+        return list;
+    }
+
+    @Test
+    public void testAddElement() {
+
+        List list = createList();
+
+        list.add("Element 0");
         list.add(null);
-        list.add("Element 4");
-        assertEquals(5, list.size());
+        list.add("Element 2");
+        assertEquals(3, list.size());
 
         assertEquals("Element 0", list.get(0));
-        assertEquals("Element 1", list.get(1));
+        assertEquals(null, list.get(1));
         assertEquals("Element 2", list.get(2));
-        assertEquals(null, list.get(3));
-        assertEquals("Element 4", list.get(4));
 
     }
 
@@ -35,45 +40,43 @@ public abstract class AbstractListTest {
         assertEquals(0, list.size());
 
         list.add("Element 0");
-        list.add("Element 1");
-        list.add("Element 2");
         list.add(null);
-        list.add("Element 4");
+        list.add("Element 2");
+        assertEquals(3, list.size());
+
+        list.add("Element New", 1);
+        assertEquals(4, list.size());
+
+        assertEquals("Element 0", list.get(0));
+        assertEquals("Element New", list.get(1));
+        assertEquals(null, list.get(2));
+        assertEquals("Element 2", list.get(3));
+
+        list.add(null, 3);
         assertEquals(5, list.size());
 
-        list.add("Element New", 2);
-        assertEquals(6, list.size());
-
         assertEquals("Element 0", list.get(0));
-        assertEquals("Element 1", list.get(1));
-        assertEquals("Element New", list.get(2));
-        assertEquals("Element 2", list.get(3));
-        assertEquals(null, list.get(4));
-        assertEquals("Element 4", list.get(5));
-
-        list.add(null, 1);
-        assertEquals(7, list.size());
-
-        assertEquals("Element 0", list.get(0));
-        assertEquals(null, list.get(1));
-        assertEquals("Element 1", list.get(2));
-        assertEquals("Element New", list.get(3));
+        assertEquals("Element New", list.get(1));
+        assertEquals(null, list.get(2));
+        assertEquals(null, list.get(3));
         assertEquals("Element 2", list.get(4));
-        assertEquals(null, list.get(5));
-        assertEquals("Element 4", list.get(6));
 
     }
 
     @Test
-    public void testAddElementMinIndexWhenThrow() {
+    public void testThrowExceptionWhenAddElementOnEmptyList() {
         List list = createList();
         assertThrows(IndexOutOfBoundsException.class, () -> {
             list.add("Element", -1);
         });
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.add("Element", list.size() + 1);
+        });
+    }
 
-        list.add("Element 0");
-        list.add("Element 1");
-        list.add("Element 2");
+    @Test
+    public void testThrowExceptionWhenAddElementBelowRange() {
+        List list = createListWithThreeElements();
 
         assertThrows(IndexOutOfBoundsException.class, () -> {
             list.add("Element", -1);
@@ -81,68 +84,48 @@ public abstract class AbstractListTest {
     }
 
     @Test
-    public void testAddElementMaxIndexWhenThrow() {
-        List list = createList();
+    public void testThrowExceptionWhenAddElementAboveRange() {
+        List list = createListWithThreeElements();
         assertThrows(IndexOutOfBoundsException.class, () -> {
             list.add("Element", list.size() + 1);
         });
 
-        list.add("Element 0");
-        list.add("Element 1");
-        list.add("Element 2");
-
-        assertThrows(IndexOutOfBoundsException.class, () -> {
-            list.add("Element", list.size() + 1);
-        });
     }
 
     @Test
     public void testRemoveElementByIndex() {
 
-        List list = createList();
-        assertEquals(0, list.size());
+        List list = createListWithThreeElements();
+        assertEquals(3, list.size());
 
-        // add 5 elements
-        list.add("Element 0");
-        list.add("Element 1");
-        list.add("Element 2");
-        list.add("Element 3");
-        list.add("Element 4");
+        Object value = list.remove(1);
+        assertEquals("Element 1", value);
 
-        assertEquals(5, list.size());
-
-        Object value = list.remove(2);
-        assertEquals("Element 2", value);
-
-        assertEquals(4, list.size());
+        assertEquals(2, list.size());
 
         assertEquals("Element 0", list.get(0));
-        assertEquals("Element 1", list.get(1));
-        assertEquals("Element 3", list.get(2));
-        assertEquals("Element 4", list.get(3));
+        assertEquals("Element 2", list.get(2));
 
 
         list.add(null);
         list.add("New Element");
         list.add(null);
 
-        assertEquals(7, list.size());
+        assertEquals(5, list.size());
 
         value = list.remove(4);
         assertNull(value);
-        assertEquals(6, list.size());
+        assertEquals(4, list.size());
 
         assertEquals("Element 0", list.get(0));
-        assertEquals("Element 1", list.get(1));
-        assertEquals("Element 3", list.get(2));
-        assertEquals("Element 4", list.get(3));
-        assertEquals("New Element", list.get(4));
-        assertEquals(null, list.get(5));
+        assertEquals("Element 2", list.get(1));
+        assertEquals(null, list.get(2));
+        assertEquals("New Element", list.get(3));
 
     }
 
     @Test
-    public void testRemoveElementMinIndexWhenThrow() {
+    public void testThrowExceptionWhenRemoveElementOnEmptyList() {
         List list = createList();
 
         assertThrows(IndexOutOfBoundsException.class, () -> {
@@ -152,9 +135,11 @@ public abstract class AbstractListTest {
             list.remove(0);
         });
 
-        list.add("Element 0");
-        list.add("Element 1");
-        list.add("Element 2");
+    }
+
+    @Test
+    public void testThrowExceptionWhenRemoveElementBelowRange() {
+        List list = createListWithThreeElements();
 
         assertThrows(IndexOutOfBoundsException.class, () -> {
             list.remove(-1);
@@ -162,20 +147,13 @@ public abstract class AbstractListTest {
     }
 
     @Test
-    public void testRemoveElementMaxIndexWhenThrow() {
-        List list = createList();
+    public void testThrowExceptionWhenRemoveElementAboveRange() {
+        List list = createListWithThreeElements();
 
         assertThrows(IndexOutOfBoundsException.class, () -> {
             list.remove(list.size() + 1);
         });
 
-        list.add("Element 0");
-        list.add("Element 1");
-        list.add("Element 2");
-
-        assertThrows(IndexOutOfBoundsException.class, () -> {
-            list.add("Element", list.size() + 1);
-        });
     }
 
     @Test
@@ -186,14 +164,10 @@ public abstract class AbstractListTest {
 
         list.add("Element 0");
         list.add("Element 1");
-        list.add("Element 2");
 
         assertFalse(list.isEmpty());
 
         list.remove(0);
-        assertFalse(list.isEmpty());
-
-        list.remove(1);
         assertFalse(list.isEmpty());
 
         list.remove(0);
@@ -205,11 +179,9 @@ public abstract class AbstractListTest {
     public void testIsEmptyWithClear() {
 
         List list = createList();
-        assertTrue(list.isEmpty());
 
         list.add("Element 0");
         list.add("Element 1");
-        list.add("Element 2");
 
         assertFalse(list.isEmpty());
 
@@ -226,7 +198,6 @@ public abstract class AbstractListTest {
 
         list.add("Element 0");
         list.add("Element 1");
-        list.add("Element 2");
 
         assertEquals(3, list.size());
 
@@ -249,7 +220,6 @@ public abstract class AbstractListTest {
 
         list.add("Element 0");
         list.add("Element 1");
-        list.add("Element 2");
 
         assertEquals(3, list.size());
 
@@ -263,17 +233,12 @@ public abstract class AbstractListTest {
 
         List list = createList();
 
-        assertTrue(list.isEmpty());
-        assertEquals(0, list.size());
-
         list.clear();
 
         assertTrue(list.isEmpty());
         assertEquals(0, list.size());
 
-        list.add("Element 0");
-        list.add("Element 1");
-        list.add("Element 2");
+        list = createListWithThreeElements();
 
         assertTrue(!list.isEmpty());
         assertEquals(3, list.size());
@@ -291,16 +256,12 @@ public abstract class AbstractListTest {
         List list = createList();
 
         list.add("Element 0");
-        list.add("Element 1");
-        list.add("Element 2");
         list.add(null);
-        list.add("Element 4");
+        list.add("Element 2");
 
         assertEquals("Element 0", list.get(0));
-        assertEquals("Element 1", list.get(1));
+        assertEquals(null, list.get(1));
         assertEquals("Element 2", list.get(2));
-        assertEquals(null, list.get(3));
-        assertEquals("Element 4", list.get(4));
 
     }
 
@@ -310,33 +271,21 @@ public abstract class AbstractListTest {
         List list = createList();
 
         list.add("Element 0");
-        list.add("Element 1");
-        list.add("Element 2");
         list.add(null);
-        list.add("Element 4");
-
-        assertEquals("Element 0", list.get(0));
-        assertEquals("Element 1", list.get(1));
-        assertEquals("Element 2", list.get(2));
-        assertEquals(null, list.get(3));
-        assertEquals("Element 4", list.get(4));
+        list.add("Element 2");
 
         assertEquals("Element 0", list.set("New Element 0", 0));
-        assertEquals("Element 1", list.set(null, 1));
-        assertEquals("Element 2", list.set("New Element 2", 2));
-        assertEquals(null, list.set("New Element 3", 3));
-        assertEquals("Element 4", list.set("New Element 4", 4));
+        assertEquals(null, list.set("New Element 1", 1));
+        assertEquals("Element 2", list.set(null, 2));
 
         assertEquals("New Element 0", list.get(0));
-        assertEquals(null, list.get(1));
-        assertEquals("New Element 2", list.get(2));
-        assertEquals("New Element 3", list.get(3));
-        assertEquals("New Element 4", list.get(4));
+        assertEquals("New Element 1", list.get(1));
+        assertEquals(null, list.get(2));
 
     }
 
     @Test
-    public void testGetElementMinIndexWhenThrow() {
+    public void testThrowExceptionWhenGetElementOnEmptyList() {
 
         List list = createList();
 
@@ -347,9 +296,12 @@ public abstract class AbstractListTest {
             list.get(0);
         });
 
-        list.add("Element 0");
-        list.add("Element 1");
-        list.add("Element 2");
+    }
+
+    @Test
+    public void testThrowExceptionWhenGetElementBelowRange() {
+
+        List list = createListWithThreeElements();
 
         assertThrows(IndexOutOfBoundsException.class, () -> {
             list.get(-1);
@@ -358,17 +310,9 @@ public abstract class AbstractListTest {
     }
 
     @Test
-    public void testGetElementMaxIndexWhenThrow() {
+    public void testThrowExceptionWhenGetElementAboveRange() {
 
-        List list = createList();
-
-        assertThrows(IndexOutOfBoundsException.class, () -> {
-            list.get(list.size() + 1);
-        });
-
-        list.add("Element 0");
-        list.add("Element 1");
-        list.add("Element 2");
+        List list = createListWithThreeElements();
 
         assertThrows(IndexOutOfBoundsException.class, () -> {
             list.get(list.size() + 1);
@@ -376,8 +320,10 @@ public abstract class AbstractListTest {
 
     }
 
+    //testThrowExceptionWhenSetElementOnEmptyList
+
     @Test
-    public void testSetElementMinIndexWhenThrow() {
+    public void testThrowExceptionWhenSetElementOnEmptyList() {
 
         List list = createList();
 
@@ -388,9 +334,12 @@ public abstract class AbstractListTest {
             list.set("Element", 0);
         });
 
-        list.add("Element 0");
-        list.add("Element 1");
-        list.add("Element 2");
+    }
+
+    @Test
+    public void testThrowExceptionWhenSetElementBelowRange() {
+
+        List list = createListWithThreeElements();
 
         assertThrows(IndexOutOfBoundsException.class, () -> {
             list.set("Element", -1);
@@ -399,17 +348,9 @@ public abstract class AbstractListTest {
     }
 
     @Test
-    public void testSetElementMaxIndexWhenThrow() {
+    public void testThrowExceptionWhenSetElementAboveRange() {
 
-        List list = createList();
-
-        assertThrows(IndexOutOfBoundsException.class, () -> {
-            list.set("Element", list.size() + 1);
-        });
-
-        list.add("Element 0");
-        list.add("Element 1");
-        list.add("Element 2");
+        List list = createListWithThreeElements();
 
         assertThrows(IndexOutOfBoundsException.class, () -> {
             list.set("Element", list.size() + 1);
@@ -423,16 +364,10 @@ public abstract class AbstractListTest {
         List list = createList();
 
         list.add("Element 0");
-        list.add("Element 1");
         list.add(null);
-        list.add("Element 3");
-        list.add("Element 4");
 
         assertEquals(0, list.indexOf("Element 0"));
-        assertEquals(1, list.indexOf("Element 1"));
-        assertEquals(2, list.indexOf(null));
-        assertEquals(3, list.indexOf("Element 3"));
-        assertEquals(4, list.indexOf("Element 4"));
+        assertEquals(1, list.indexOf(null));
 
         assertEquals(-1, list.indexOf("Element Zero"));
 
@@ -443,27 +378,13 @@ public abstract class AbstractListTest {
 
         List list = createList();
 
-        list.add("Element 0");
-        list.add("Element 1");
         list.add("Element Copy");
-        list.add("Element 3");
-        list.add("Element Copy");
-        list.add("Element Copy");
-        list.add("Element 6");
-        list.add("Element 7");
         list.add(null);
+        list.add("Element Copy");
         list.add(null);
-        list.add("Element 10");
 
-        assertEquals(0, list.indexOf("Element 0"));
-        assertEquals(1, list.indexOf("Element 1"));
-        assertEquals(2, list.indexOf("Element Copy"));
-        assertEquals(3, list.indexOf("Element 3"));
-        assertEquals(6, list.indexOf("Element 6"));
-        assertEquals(7, list.indexOf("Element 7"));
-        assertEquals(8, list.indexOf(null));
-        assertEquals(10, list.indexOf("Element 10"));
-
+        assertEquals(0, list.indexOf("Element Copy"));
+        assertEquals(1, list.indexOf(null));
     }
 
     @Test
@@ -472,15 +393,9 @@ public abstract class AbstractListTest {
         List list = createList();
 
         list.add("Element 0");
-        list.add("Element 1");
         list.add(null);
-        list.add("Element 3");
-        list.add("Element 4");
 
-        assertEquals(4, list.lastIndexOf("Element 4"));
-        assertEquals(3, list.lastIndexOf("Element 3"));
-        assertEquals(2, list.lastIndexOf(null));
-        assertEquals(1, list.lastIndexOf("Element 1"));
+        assertEquals(1, list.lastIndexOf(null));
         assertEquals(0, list.lastIndexOf("Element 0"));
 
         assertEquals(-1, list.lastIndexOf("Element Zero"));
@@ -492,26 +407,13 @@ public abstract class AbstractListTest {
 
         List list = createList();
 
-        list.add("Element 0");
-        list.add("Element 1");
         list.add("Element Copy");
-        list.add("Element 3");
-        list.add("Element Copy");
-        list.add("Element Copy");
-        list.add("Element 6");
-        list.add("Element 7");
         list.add(null);
+        list.add("Element Copy");
         list.add(null);
-        list.add("Element 10");
 
-        assertEquals(10, list.lastIndexOf("Element 10"));
-        assertEquals(9, list.lastIndexOf(null));
-        assertEquals(7, list.lastIndexOf("Element 7"));
-        assertEquals(6, list.lastIndexOf("Element 6"));
-        assertEquals(5, list.lastIndexOf("Element Copy"));
-        assertEquals(3, list.lastIndexOf("Element 3"));
-        assertEquals(1, list.lastIndexOf("Element 1"));
-        assertEquals(0, list.lastIndexOf("Element 0"));
+        assertEquals(3, list.lastIndexOf(null));
+        assertEquals(2, list.lastIndexOf("Element Copy"));
 
     }
 
@@ -521,17 +423,10 @@ public abstract class AbstractListTest {
         List list = createList();
 
         list.add("Element 0");
-        list.add("Element 1");
         list.add(null);
-        list.add("Element 3");
-        list.add("Element 4");
 
         assertTrue(list.contains("Element 0"));
-        assertTrue(list.contains("Element 1"));
         assertTrue(list.contains(null));
-        assertTrue(list.contains("Element 3"));
-        assertTrue(list.contains("Element 4"));
-
         assertFalse(list.contains("Element Zero"));
 
     }
