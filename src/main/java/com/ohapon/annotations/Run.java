c35042edc6a6f97b5172d6f3a53cc1202af89898
Oleh <ohapon@Olehs-MacBook-Pro.local>
2021-07-14 13:16:16 +0300

    Add annotations

---
+++ src/main/java/com/ohapon/annotations/Run.java
@@ -0,0 +1,14 @@
+package com.ohapon.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Target({ElementType.METHOD})
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Run {
+}
