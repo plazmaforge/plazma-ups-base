ad5c7705461c388a029c5c4eafe7c5ad5657f5d5
Oleh <ohapon@Olehs-MacBook-Pro.local>
2021-07-23 11:44:07 +0300

    Refactored

--- src/main/java/com/ohapon/datastructures/map/HashMap.java
+++ src/main/java/com/ohapon/datastructures/map/HashMap.java
@@ -6,31 +6,33 @@ import java.util.Objects;
 
 public class HashMap<K, V> implements Map<K, V> {
 
-    private Bucket<K, V>[] buckets = new Bucket[10];
+    private static final int DEFAULT_INITIAL_CAPACITY = 10;
+
+    private Bucket<K, V>[] buckets = new Bucket[DEFAULT_INITIAL_CAPACITY];
     private int size;
 
     @Override
     public V put(K key, V value) {
         Bucket<K, V> bucket = findBucketWithCreate(key);
-        return bucket.put(key, value);
+        return put(bucket.entries, key, value);
     }
 
     @Override
     public V get(K key) {
         Bucket<K, V> bucket = findBucket(key);
-        return bucket == null ? null : bucket.get(key);
+        return bucket == null ? null : get(bucket.entries, key);
     }
 
     @Override
     public boolean containsKey(K key) {
         Bucket<K, V> bucket = findBucket(key);
-        return bucket == null ? false : bucket.containsKey(key);
+        return bucket == null ? false : containsKey(bucket.entries, key);
     }
 
     @Override
     public V remove(K key) {
         Bucket<K, V> bucket = findBucket(key);
-        return bucket == null ? null : bucket.remove(key);
+        return bucket == null ? null : remove(bucket.entries, key);
     }
 
     @Override
@@ -45,7 +47,7 @@ public class HashMap<K, V> implements Map<K, V> {
 
     private int getIndex(K key) {
         int hash = key == null ? 0 : key.hashCode();
-        return hash % buckets.length;
+        return Math.abs(hash) % buckets.length;
     }
 
     private Bucket<K, V> findBucket(K key) {
@@ -92,52 +94,53 @@ public class HashMap<K, V> implements Map<K, V> {
         public Bucket() {
         }
 
-        private Entry<K, V> findEntry(K key) {
-            Entry<K, V> entry = null;
-            for (int i = 0; i < entries.size(); i++) {
-                entry = entries.get(i);
-                if (Objects.equals(key, entry.getKey())) {
-                    return entry;
-                }
+    }
+
+    private Entry<K, V> findEntry(List<Entry<K, V>> entries, K key) {
+        Entry<K, V> entry = null;
+        for (int i = 0; i < entries.size(); i++) {
+            entry = entries.get(i);
+            if (Objects.equals(key, entry.getKey())) {
+                return entry;
             }
+        }
+        return null;
+    }
+
+    private V put(List<Entry<K, V>> entries, K key, V value) {
+        Entry<K, V> entry = findEntry(entries, key);
+        V oldValue = null;
+        if (entry != null) {
+            oldValue = entry.getValue();
+            entry.setValue(value);
+        } else {
+            entry = new Entry<>(key, value);
+            entries.add(entry);
+            size++;
+        }
+        return oldValue;
+    }
+
+    private V get(List<Entry<K, V>> entries, K key) {
+        Entry<K, V> entry = findEntry(entries, key);
+        return entry == null ? null : entry.getValue();
+    }
+
+    private boolean containsKey(List<Entry<K, V>> entries, K key) {
+        Entry<K, V> entry = findEntry(entries, key);
+        return entry != null;
+    }
+
+    private V remove(List<Entry<K, V>> entries, K key) {
+        Entry<K, V> entry = findEntry(entries, key);
+        if (entry == null) {
             return null;
         }
-
-        public V put(K key, V value) {
-            Entry<K, V> entry = findEntry(key);
-            V oldValue = null;
-            if (entry != null) {
-                oldValue = entry.getValue();
-                entry.setValue(value);
-            } else {
-                entry = new Entry<>(key, value);
-                entries.add(entry);
-                size++;
-            }
-            return oldValue;
-        }
-
-        public V get(K key) {
-            Entry<K, V> entry = findEntry(key);
-            return entry == null ? null : entry.getValue();
-        }
-
-        public boolean containsKey(K key) {
-            Entry<K, V> entry = findEntry(key);
-            return entry != null;
-        }
-
-        public V remove(K key) {
-            Entry<K, V> entry = findEntry(key);
-            if (entry == null) {
-                return null;
-            }
-            V oldValue = entry.getValue();
-            entries.remove(entry);
-            size--;
-            return oldValue;
-        }
-
+        V oldValue = entry.getValue();
+        entries.remove(entry);
+        size--;
+        return oldValue;
     }
+
 
 }
